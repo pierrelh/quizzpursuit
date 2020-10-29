bf0437fc2a7051e0324527fa9b65978334e1d87c
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-10-29 14:51:20 +0100

    Created pages and css files

---
+++ pages/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Ouiches</title>
+        <link rel="stylesheet" type="text/css" href="styles/common/master.css"/>
+        <link rel="stylesheet" type="text/css" href="styles/common/header.css"/>
+        <link rel="stylesheet" type="text/css" href="styles/common/quizz.css"/>
+        <link rel="stylesheet" type="text/css" href="styles/common/index.css"/>
+        
+        <link href="https://fonts.googleapis.com/css2?family=Faster+One&display=swap" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="//cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css"> -->
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+    
+    <body>
+
+        <div class="page-logo">
+            <h1>Quizz Pursuit</h1>
+        </div>
