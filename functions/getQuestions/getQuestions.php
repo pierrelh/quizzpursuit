cf15061879ef7c0f881e40116619bab8d066c981
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-10-29 14:30:12 +0100

    add getQuestions request

---
+++ functions/getQuestions/getQuestions.php
@@ -0,0 +1,17 @@
+<?php
+
+    include_once($_SERVER['DOCUMENT_ROOT']."/functions/connexion.php");
+    $db = connect();
+
+    $sqlRequest = "SELECT *
+                   FROM quizz_questions
+                   ORDER BY word_id ASC";
+    $result = pg_query($db, $sqlRequest);
+    if (!empty($result)) {
+    $val = pg_fetch_all($result);
+    print json_encode($val);
+    }else {
+        print "false";
+    }
+
+?>
