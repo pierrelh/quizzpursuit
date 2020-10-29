2033c373ac57aa114c0af2196c07f0b53189e426
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-10-29 16:21:26 +0100

    Structure des pages

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
   require __DIR__ . '/pages/header.php';
-  require __DIR__ . '/pages/quizz.php';
   require __DIR__ . '/pages/index.php';
   
 ?>
---
+++ pages/quizz.php
@@ -0,0 +1,19 @@
+<?php require __DIR__ . '/header.php'; ?>
+
+        <div class="quizz-body">
+
+            <div class="question">Question blablabla lorem ipsum dolor machin</div>
+
+            <div class="answers">
+                <ul class=answer-list>
+                    <li>Réponse 1</li>
+                    <li>Réponse 2</li>
+                    <li>Réponse 3</li>
+                    <li>Réponse 4</li>
+                </ul>
+            </div>
+            
+        </div>
+
+    </body>
+</html>
