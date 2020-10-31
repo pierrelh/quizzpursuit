d5c66a95b23b314267d6a1e576b7edcc92aec988
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-10-31 15:39:34 +0100

    working on priting getAllQuestions

--- api/questions/getAll/index.php
+++ api/questions/getAll/index.php
@@ -11,7 +11,7 @@
         $data = pg_fetch_all($result);
 
         include_once($_SERVER['DOCUMENT_ROOT']."/api/map/question.php");
-            array_map('mapQuestion', $data);
+        array_map('mapQuestion', $data);
         print json_encode($data);
     }else {
         print "false";
