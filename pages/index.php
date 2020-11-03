95c2b14281bb4e2a6e06e6d5228ea77f81b3084d
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-11-03 14:37:48 +0100

    CSS changes

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,7 @@
         <div class="homepage-body">
 
             <div class="start-button">
+                <div class="image start-image"></div>
                 <a href="<?php echo $link ?>/quizz">Lancer un quizz</a>
             </div>
 
