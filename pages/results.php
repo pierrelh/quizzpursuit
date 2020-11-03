d1ad410d603d3240d44543e5e2096857d1022ef3
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-11-03 17:51:37 +0100

    Result page added

---
+++ pages/results.php
@@ -0,0 +1,19 @@
+
+        <div class="results-body">
+
+            <div class="score-section">
+                <p>Votre score</p>
+                <div class="score"></div>
+            </div>
+
+            <div class="username-section">
+                <form action="submit">
+                    <label for=""></label>
+                    <input type="text">                
+                </form>
+            </div>
+            
+        </div>
+
+    </body>
+</html>
