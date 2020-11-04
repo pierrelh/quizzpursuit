65f6fc41d0e19764fb30d5e07fd83054c70f228a
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-11-04 14:24:52 +0100

    work on API getting all or some users score

--- api/functions/getParameter.php
+++ api/functions/getParameter.php
@@ -5,7 +5,7 @@ function getParameter(){
   $url = explode(':', $url);
   $parameter = array_pop($url);
   if(preg_match('#/api/questions#', $parameter) || $parameter == "" || !is_numeric($parameter)){
-      $parameter = null;
+      $parameter = false;
   }
   return $parameter;
 }
