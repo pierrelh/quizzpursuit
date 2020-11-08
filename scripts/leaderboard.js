f0228e85e0cda631c23b9f0836896e55aa1a158a
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-11-08 12:27:43 +0100

    work on getting leaderboard

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -14,8 +14,7 @@ fetch(server + "/functions/score/getLeaderboard.php")
 
           // Create <li> element
           var li = document.createElement("li");
-          var classement = +key + 1;
-          li.innerHTML = "#" + classement + " " + response[key].username + " - " + response[key].score + "%"
+          li.innerHTML = "#" + (key+1) + " " + response[key].username + " - " + response[key].score + "%"
           leaderBoardList.appendChild(li);
 
         }
