d1c5a6849509b11016429dca4a5de4e97a225579
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-11-13 12:53:01 +0100

    work on priting others quizzs questions

--- scripts/get-other-quizz.js
+++ scripts/get-other-quizz.js
@@ -1,78 +1,83 @@
 // Fetch data from PHP file
 var server = "https://" + window.location.hostname;
 fetch(server + "/api/out/questions/getQuestions.php")
-  .then((response) => response.json())
-  .then(function (response) {
+    .then((response) => response.json())
+    .then(function (response) {
 
-    console.log(response);
+        console.log(response);
 
-    // Loop in json
-    var key = 0;
-    var count = 0
-    var val;
-    var radios;
+        // Loop in json
+        var key = 0;
+        var count = 0
+        var val;
+        var radios;
 
-    addQuestion();
-    
-    document.getElementById("nextButton").addEventListener("click", function(event){
-      form = document.getElementById("quizform");
-      // get list of radio buttons with specified name
-      radios = form.elements["quiz"];
+        addQuestion();
         
-      // loop through list of radio buttons
-      for (var i=0, len=radios.length; i<len; i++) {
-          if ( radios[i].checked ) { // radio checked?
-              val = radios[i].value; // if so, hold its value in val
-              radios[i].checked = false; //reseting the checkmark
-              break; // and break out of for loop
-          }
-      }
-      if (val == response[key].answer) {
-        count++
-      }
-      key++
-      if (key == response.length) {
-        var score = count / key * 100;
-        //Create the cookie to remember the user's username
-        let date = new Date(Date.now() + 3600);
-        date = date.toUTCString();
-        
-        document.cookie = "SESSION_SCORE=" + score.toString() + "; SameSite=Lax; path=/; expires=" + date;
-        
-        //Redirect the user to the main page
-        window.location.assign(server + "/other-results");
-        
-      }
-      addQuestion();
-    });
-      
-    function addQuestion()
-    {
-        if (response.hasOwnProperty(key)) {
-            // Replace dummy question with current question in loop
-            document.getElementById('quizzQuestion').innerHTML = response[key].question;
+        document.getElementById("nextButton").addEventListener("click", function(event){
+            form = document.getElementById("quizform");
+            // get list of radio buttons with specified name
+            radios = form.elements["quiz"];
+                
+            // loop through list of radio buttons
+            for (var i=0, len=radios.length; i<len; i++) {
+                if ( radios[i].checked ) { // radio checked?
+                    val = parseInt(radios[i].value) - 1; // if so, hold its value in val
+                    radios[i].checked = false; //reseting the checkmark
+                    break; // and break out of for loop
+                }
+            }
 
             var answers = response[key].answers;
-            for (let index = 0; index < answers.length; index++) {
-                var id = "reponse" + (index + 1);
-                document.getElementById(id).innerText = answers[index].answer;
+            if (answers[val].isCorrect) {
+                console.log("correct");
+                count++
+            }
+
+            key++
+            if (key == response.length) {
+                var score = count / key * 100;
+                //Create the cookie to remember the user's username
+                let date = new Date(Date.now() + 3600);
+                date = date.toUTCString();
+                
+                document.cookie = "SESSION_SCORE=" + score.toString() + "; SameSite=Lax; path=/; expires=" + date;
+                
+                //Redirect the user to the main page
+                window.location.assign(server + "/other-results");
                 
             }
-        //   // First answer
-        //   document.getElementById("reponse1").innerText = response[key].first_choice;
+            
+            addQuestion();
+        });
+        
+        function addQuestion()
+        {
+            if (response.hasOwnProperty(key)) {
+                // Replace dummy question with current question in loop
+                document.getElementById('quizzQuestion').innerHTML = response[key].question;
 
-        //   // Second answer
-        //   document.getElementById("reponse2").innerText = response[key].second_choice;
+                var answers = response[key].answers;
+                for (let index = 0; index < answers.length; index++) {
+                    var id = "reponse" + (index + 1);
+                    document.getElementById(id).innerText = answers[index].answer;
+                    
+                }
+            //   // First answer
+            //   document.getElementById("reponse1").innerText = response[key].first_choice;
 
-        //   // Third answer
-        //   document.getElementById("reponse3").innerText = response[key].third_choice;
+            //   // Second answer
+            //   document.getElementById("reponse2").innerText = response[key].second_choice;
 
-        //   // Fourth answer      
-        //   document.getElementById("reponse4").innerText = response[key].fourth_choice;
+            //   // Third answer
+            //   document.getElementById("reponse3").innerText = response[key].third_choice;
 
-        }
+            //   // Fourth answer      
+            //   document.getElementById("reponse4").innerText = response[key].fourth_choice;
 
-      }    
+            }
+
+        }    
      
   });
 
