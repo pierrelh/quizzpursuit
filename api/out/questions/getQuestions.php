a65064ebabd92992f53bb4f765d3fa42e3b64f5e
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-11-15 13:51:32 +0100

    implement polar ocean api

--- api/out/questions/getQuestions.php
+++ api/out/questions/getQuestions.php
@@ -1,13 +1,14 @@
 <?php
 
-    $links = [/*'https://polar-ocean-73785.herokuapp.com/api/questions/3',*/
-              'https://wsf-popcorn-backend.herokuapp.com/api/questions/5',
-              'https://adley-quizz.herokuapp.com/api/questions/5'];
+    $links = ['https://polar-ocean-73785.herokuapp.com/api/questions/3',
+              'https://wsf-popcorn-backend.herokuapp.com/api/questions/4',
+              'https://adley-quizz.herokuapp.com/api/questions/3'];
     $questions = [];
     foreach ($links as $value) {
         $raw = file_get_contents($value);
         $json = json_decode($raw);
         $result = get_object_vars($json);
+        $result = $result['questions'];
         foreach ($result as $key => $value) {
             foreach ($value as $value) {
                 array_push($questions, get_object_vars($value));
